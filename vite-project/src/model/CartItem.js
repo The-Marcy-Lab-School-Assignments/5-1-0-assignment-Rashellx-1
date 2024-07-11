cb8e02185282b27626d7ea2cde04f6b43a30cf84
Rashell Aldas <rashellaldas@rashells-mbp.mynetworksettings.com>
2024-07-11 02:28:53 -0400

    In progress

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -1,7 +1,11 @@
 import getId from "../utils/getId";
 
 class CartItem {
-
+  constructor(id, name, price) {
+    this.id = id
+    this.name = name
+    this.price = price
+  }
 }
 
 export default CartItem;
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,18 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
-
+  #cartItems = []
+  constructor(id, cartItems) {
+    this.id = id
+    this.#cartItems = cartItems
+  }
+  createItem(name, price) {
+    this.name = name
+    this.price = price
+  }
+  getItems() {
+    return [...this.#cartItems]
+  }
 }
 
 export default ShoppingCart;
