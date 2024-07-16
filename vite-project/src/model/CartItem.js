9fe5c611ba013250857e90b068c6ea6187313e13
Rashell Aldas <rashellaldas@Rashells-MacBook-Pro.local>
2024-07-16 16:03:10 -0400

    updated

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -2,7 +2,7 @@ import getId from "../utils/getId";
 
 class CartItem {
   constructor(name, price) {
-    this.id = getId()
+    this.id = getId()  //invoking id method from getId js 
     this.name = name
     this.price = price
   }
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -9,16 +9,16 @@ class ShoppingCart {
     this.#cartItems = []
   }
   createItem(name, price) {
-    const newItem = new CartItem(name, price)
+    const newItem = new CartItem(name, price) // create a variable to hold the newItem
     console.log(newItem)
-    this.#cartItems.push(newItem)
-    return newItem
+    this.#cartItems.push(newItem) // adding this new item in #cartItems array directly
+    return newItem                // returing the newItem
   }
   getItems() {
-    return [...this.#cartItems]
+    return [...this.#cartItems] //creating shallow copy of #cartItems array
   }
   removeItem(id) {
-    this.#cartItems.splice(id)
+    this.#cartItems.splice(this.#cartItems.findIndex((item) => item.id === id), 1)
   }
 
 }
